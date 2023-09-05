35bd8f1bfbe0d6ba09da1b67ebf79ed930367cc8
Jan-Victor Björkqvist <janvb@met.no>
2023-09-05 15:15:55 +0200

    distance function return distance in m not km

--- distance_functions.py
+++ distance_functions.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def min_distance(lon, lat, lon_vec, lat_vec) -> tuple[float, int]:
-    """Calculates minimum distance [km] between a given point and a list of
+    """Calculates minimum distance [m] between a given point and a list of
     points given in spherical coordinates (lon/lat degrees).
 
     Also returns index of the found minimum.
@@ -16,25 +16,25 @@ def min_distance(lon, lat, lon_vec, lat_vec) -> tuple[float, int]:
 
 
 def min_cartesian_distance(x, y, x_vec, y_vec) -> tuple[float, int]:
-    """ "Calculates minimum distance [km] between a given point and list of points given
+    """ "Calculates minimum distance [m] between a given point and list of points given
     in cartesian coordinates [m].
 
     Also returns incex of found minimum"""
     dx = ((y - y_vec) ** 2 + (x - x_vec) ** 2) ** 0.5
 
-    return dx.min() / 1000, dx.argmin()
+    return dx.min(), dx.argmin()
 
 
 def lon_in_km(lat: float) -> float:
     """Converts one longitude degree to km for a given latitude."""
 
-    return distance_2points(lat, 0, lat, 1)
+    return distance_2points(lat, 0, lat, 1) / 1000
 
 
 def lat_in_km(lat: float) -> float:
     """Converts one latitude degree to km for a given latitude."""
 
-    return distance_2points(lat, 0, lat + 1, 0)
+    return distance_2points(lat, 0, lat + 1, 0) / 1000
 
 
 def domain_size_in_km(
@@ -42,13 +42,14 @@ def domain_size_in_km(
 ) -> tuple[float, float]:
     """Calculates approximate size of grid in km."""
 
-    km_x = distance_2points(
-        (lat[0] + lat[1]) / 2, lon[0], (lat[0] + lat[1]) / 2, lon[1]
+    km_x = (
+        distance_2points((lat[0] + lat[1]) / 2, lon[0], (lat[0] + lat[1]) / 2, lon[1])
+        / 1000
     )
-    km_y = distance_2points(lat[0], lon[0], lat[1], lon[0])
+    km_y = distance_2points(lat[0], lon[0], lat[1], lon[0]) / 1000
     return km_x, km_y
 
 
 def distance_2points(lat1, lon1, lat2, lon2) -> float:
-    """Calculate distance between two points"""
-    return geopy.distance.geodesic((lat1, lon1), (lat2, lon2)).km
+    """Calculate distance between two points in m"""
+    return geopy.distance.geodesic((lat1, lon1), (lat2, lon2)).m
