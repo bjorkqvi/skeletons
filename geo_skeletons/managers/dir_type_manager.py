5e58464864ce81910683056de7c5158aac392cd9
Jan-Victor Björkqvist <janvb@met.no>
2024-05-06 13:32:36 +0200

    type hinting and docs

--- geo_skeletons/managers/dir_type_manager.py
+++ geo_skeletons/managers/dir_type_manager.py
@@ -3,11 +3,12 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from .coordinate_manager import CoordinateManager
+    import dask.array as da
 
 from .dask_manager import DaskManager
 import numpy as np
 import xarray as xr
-
+from typing import Union
 from geo_skeletons import dask_computations
 
 OFFSET = {"from": 180, "to": 0}
@@ -18,6 +19,11 @@ class DirTypeManager:
         self.coord_manager = coord_manager
 
     def convert(self, data, in_type: str, out_type: str):
+        """Converts one directional type to another.
+
+        Possible types are:
+        'from', 'to' and 'math'
+        """
         if out_type is None:
             return data
         if in_type is None:
@@ -39,6 +45,7 @@ class DirTypeManager:
 
     @staticmethod
     def convert_to_math_dir(data, dir_type: str):
+        """Converts data to mathematical convetion (radians, east=0, north = pi/2)"""
         if dir_type == "math":  # Convert to mathematical convention
             return data
         math_dir = (90 - data + OFFSET[dir_type]) * np.pi / 180
@@ -54,6 +61,7 @@ class DirTypeManager:
 
     @staticmethod
     def convert_from_math_dir(data, dir_type: str):
+        """Converts data from mathematical convention"""
         if dir_type == "math":
             return data
 
@@ -61,13 +69,23 @@ class DirTypeManager:
         return dask_computations.mod(data, 360)
 
     @staticmethod
-    def compute_magnitude(x, y):
+    def compute_magnitude(
+        x: Union[np.ndarray, da.array, xr.DataArray],
+        y: Union[np.ndarray, da.array, xr.DataArray],
+    ) -> Union[np.ndarray, da.array, xr.DataArray]:
+        """Computes magnitudes (norms) of two variables"""
         if x is None or y is None:
             return None
         return (x**2 + y**2) ** 0.5
 
     @staticmethod
-    def compute_math_direction(x, y):
+    def compute_math_direction(
+        x: Union[np.ndarray, da.array, xr.DataArray],
+        y: Union[np.ndarray, da.array, xr.DataArray],
+    ) -> Union[np.ndarray, da.array, xr.DataArray]:
+        """Computes direcetion of two variables.
+
+        Result given in mathemetical convention (radians, east=0, north = pi/2)"""
         if x is None or y is None:
             return None
 
