698499071464aa5a155ca522aa85936b9bd9401d
Jan-Victor Björkqvist <janvb@met.no>
2025-10-29 17:16:00 +0100

    dont regrid empty variables

--- geo_skeletons/managers/resample/scipy_regridders.py
+++ geo_skeletons/managers/resample/scipy_regridders.py
@@ -30,46 +30,48 @@ def scipy_griddata(data, new_grid, new_data, verbose, method: str ='nearest', dr
 
     for var_name in data.core.data_vars('all'):
         if var_name not in ['x','y','lon','lat']:
-            var = data.core.get(var_name)
-            var_coords = data.core.coords(var.coord_group)
-            if var_coords == spatial_coords:
-                if verbose:
-                    print(f"'{var_name}' {var_coords}: Regridding...")
-                new_array = griddata(points, data.get(var_name).flatten(), (target_lon, target_lat), method=method)
-                new_data.set(var_name, new_array)
-            elif set(spatial_coords + ['time']) == set(var_coords) and 'time' in new_data.core.coords():
-                if verbose:
-                    print(f"'{var_name}' {var_coords}: Regridding over time.", end='')
-                new_array = np.empty(new_data.shape(var_name))
-                Nt = len(data.time())
-                ct = 0
-                for t in range(Nt):
-                    if ct > Nt/10:
-                        print('.', end='')
-                        ct = 0
+            var = data.core.get(var_name, strict=True)
+            if var is not None:
+                var_coords = data.core.coords(var.coord_group)
+                if var_coords == spatial_coords:
+                    if verbose:
+                        print(f"'{var_name}' {var_coords}: Regridding...")
+                    new_array = griddata(points, data.get(var_name).flatten(), (target_lon, target_lat), method=method)
+                    new_data.set(var_name, new_array)
+                elif set(spatial_coords + ['time']) == set(var_coords) and 'time' in new_data.core.coords():
+                    if verbose:
+                        print(f"'{var_name}' {var_coords}: Regridding over time.", end='')
+                    new_array = np.empty(new_data.shape(var_name))
+                    Nt = len(data.time())
+                    ct = 0
+                    for t in range(Nt):
+                        if ct > Nt/10:
+                            if verbose:
+                                print('.', end='')
+                            ct = 0
 
-                    # Flatten the data for the current time step
-                    source_values = data.get(var_name)[t, ...].flatten()
-                    if drop_nan:
-                        mask = np.logical_not(np.isnan(source_values))
-                        source_values = source_values[mask]
-                        
-                        points = all_points[mask]
-                    elif mask_nan is not None:
-                        mask = np.isnan(source_values)
-                        source_values[mask] = mask_nan
-                        points = all_points
-                    else:
-                        points = all_points
-                    # Interpolate to the target grid
-                    new_array[t, ...] = griddata(points, source_values, (target_lon, target_lat), method=method)
-                    ct += 1
-                new_data.set(var_name, new_array)
-                if verbose:
-                    print('')
-            else:
-                if verbose:
-                    print(f"'{var_name}' {var_coords}: Skipping!")
+                        # Flatten the data for the current time step
+                        source_values = data.get(var_name)[t, ...].flatten()
+                        if drop_nan:
+                            mask = np.logical_not(np.isnan(source_values))
+                            source_values = source_values[mask]
+                            
+                            points = all_points[mask]
+                        elif mask_nan is not None:
+                            mask = np.isnan(source_values)
+                            source_values[mask] = mask_nan
+                            points = all_points
+                        else:
+                            points = all_points
+                        # Interpolate to the target grid
+                        new_array[t, ...] = griddata(points, source_values, (target_lon, target_lat), method=method)
+                        ct += 1
+                    new_data.set(var_name, new_array)
+                    if verbose:
+                        print('')
+                else:
+                    if verbose:
+                        print(f"'{var_name}' {var_coords}: Skipping!")
             
 
     return new_data
