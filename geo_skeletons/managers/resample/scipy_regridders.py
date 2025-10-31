61538d485ccd0b7816063e762650b3ac5c01e079
Jan-Victor Björkqvist <janvb@met.no>
2025-10-31 14:51:08 +0100

    faster regridding for gridded data sets

--- geo_skeletons/managers/resample/scipy_regridders.py
+++ geo_skeletons/managers/resample/scipy_regridders.py
@@ -1,13 +1,114 @@
-from scipy.interpolate import griddata
+from scipy.interpolate import griddata, RegularGridInterpolator
 import numpy as np
 from geo_skeletons.errors import GridError
+from copy import copy
 
 
-def scipy_griddata(data, new_grid, new_data, verbose, method: str ='nearest', drop_nan: bool=False, mask_nan: float=None,**kwargs):
-    """Uses a simple scipy griddata to regrid gridded data to gridded data.
+def scipy_regrid_gridded_data(data, new_grid, new_data, verbose, method: str='nearest', drop_nan: bool=False, mask_nan: float=None,**kwargs):
+    """Regrids gridded data using RegularGridInterpolator, which is faster than griddata"""
     
-    Can only interpolate spatial data for now (not time variable allowed)."""
+    if not data.is_gridded():
+        raise Exception('Input data needs to be gridded!')
+    
+    if verbose:
+        print(f"Using method '{method}'")
 
+    # Needs to use native here since it has to be regularly gridded
+    x, y = data.lon(native=True), data.lat(native=True)
+    if 'time' in data.core.coords():
+        t = data.time()
+        t = np.array((t-t[0]).total_seconds()).astype(int)
+
+    spatial_coords = data.core.coords('spatial')
+    
+    if new_data.core.x_str == data.core.x_str and new_data.is_gridded(): # Go from gridded to gridded
+        query_gridded = True
+        xq, yq = new_data.lon(native=True), new_data.lat(native=True)
+        
+        T, Y, X = np.meshgrid(t, yq, xq, indexing="ij")  # Use "ij" indexing for (t, y, x) order
+        query_points_time = np.column_stack([T.ravel(), Y.ravel(), X.ravel()])
+        
+        Y, X = np.meshgrid(yq, xq, indexing="ij")  # Use "ij" indexing for (t, y, x) order
+        query_points = np.column_stack([Y.ravel(), X.ravel()])   
+    else: # We need to get the query points in the native coordinates of the original data, which will make is non-gridded
+        query_gridded = False
+        if data.core.is_cartesian():
+            xq, yq = new_data.xy()
+        else:
+            xq, yq = new_data.lonlat()
+        
+            query_points = np.column_stack((yq, xq))
+
+            t_repeated = np.repeat(t, len(xq)) 
+            xq_tiled = np.tile(xq, len(t))  
+            yq_tiled = np.tile(yq, len(t)) 
+
+            query_points_time = np.column_stack((t_repeated, yq_tiled, xq_tiled))
+            
+            
+    # Check that we are not out of bounds, since RegularGridInterpolator can't handle that
+    # We also can't drop nan values and still keep data gridded
+    if min(xq) < min(x) or max(xq) > max(x) or min(yq) < min(y) or max(yq) > max(y):
+        print(f"Data ({data.core.x_str}={data.edges(data.core.x_str)}, {data.core.y_str}={data.edges(data.core.y_str)}) doesnt cover new grid ({data.core.x_str}=({min(xq):.4f}, {max(xq):.4f}, {data.core.y_str}=({min(yq):.4f}, {max(yq):.4f})). Redirecting interpolation to {scipy_regrid_point_data}")
+        new_data = scipy_regrid_point_data(data, new_grid, new_data, verbose, method=method, drop_nan=drop_nan, mask_nan=mask_nan,**kwargs)
+        return new_data
+    elif drop_nan:
+        print('Cannot drop nans and keep data gridded. Redirecting interpolation to {scipy_regrid_point_data}')
+        new_data = scipy_regrid_point_data(data, new_grid, new_data, verbose, method=method, drop_nan=drop_nan, mask_nan=mask_nan,**kwargs)
+        return new_data
+    if verbose:
+        if mask_nan is not None:
+            print(f"Replacing nan values with {mask_nan}")
+
+
+    for var_name in data.core.data_vars('all'):
+            if data.get(var_name, strict=True) is not None:
+                var = data.core.get(var_name)
+                var_coords = data.core.coords(var.coord_group)
+                if var_coords == spatial_coords:
+                    source_values = data.get(var_name)
+                    if mask_nan is not None:
+                        source_values = copy(source_values)
+                        mask = np.isnan(source_values)
+                        source_values[mask] = mask_nan
+                    if verbose:
+                        print(f"'{var_name}' {var_coords}: Regridding...")
+                    interpolator = RegularGridInterpolator((y, x), source_values, method=method)
+                    interpolated_values = interpolator(query_points)
+                    if query_gridded:
+                        interpolated_values_grid = interpolated_values.reshape((len(yq), len(xq)))
+                    elif new_data.is_gridded():
+                        interpolated_values_grid = interpolated_values.reshape((new_data.ny(), new_data.nx()))
+                    else:
+                        interpolated_values_grid = interpolated_values.reshape((len(yq),1))
+                    new_data.set(var_name, interpolated_values_grid)
+                elif set(spatial_coords + ['time']) == set(var_coords) and 'time' in new_data.core.coords():
+                    source_values = data.get(var_name)
+                    if mask_nan is not None:
+                        source_values = copy(source_values)
+                        mask = np.isnan(source_values)
+                        source_values[mask] = mask_nan
+                    if verbose:
+                        print(f"'{var_name}' {var_coords}: Regridding over time...")
+                    interpolator = RegularGridInterpolator((t, y, x), source_values, method=method)
+                    interpolated_values = interpolator(query_points_time)
+                    if query_gridded:
+                        interpolated_values_grid = interpolated_values.reshape((len(t), len(yq), len(xq)))
+                    elif new_data.is_gridded:
+                        interpolated_values_grid = interpolated_values.reshape((len(t), new_data.ny(), new_data.nx()))
+                    new_data.set(var_name, interpolated_values_grid)
+                else:
+                    if verbose:
+                        print(f"'{var_name}' {var_coords}: Skipping!")
+
+    return new_data
+
+
+
+def scipy_regrid_point_data(data, new_grid, new_data, verbose, method: str ='nearest', drop_nan: bool=False, mask_nan: float=None,**kwargs):
+    """Uses a simple scipy griddata to regrid non-gridded data."""
+    if verbose:
+        print(f"Using method '{method}'")
     # Determine the coordinates
     if new_grid.is_gridded():
         target_lon, target_lat = new_grid.longrid(native=True), new_grid.latgrid(native=True)
@@ -76,4 +177,4 @@ def scipy_griddata(data, new_grid, new_data, verbose, method: str ='nearest', dr
 
     return new_data
 
-scipy_regridders = {'gridded_to_gridded': scipy_griddata,'point_to_gridded': scipy_griddata, 'gridded_to_point': scipy_griddata, 'point_to_point': scipy_griddata,'available': True, 'installation': 'Native (default)', 'options': 'drop_nan: bool, mask_nan: float'}
+scipy_regridders = {'gridded_to_gridded': scipy_regrid_gridded_data,'point_to_gridded': scipy_regrid_point_data, 'gridded_to_point': scipy_regrid_gridded_data, 'point_to_point': scipy_regrid_point_data,'available': True, 'installation': 'Native (default)', 'options': 'drop_nan: bool, mask_nan: float'}
--- tests/test_resample/test_scipy.py
+++ tests/test_resample/test_scipy.py
@@ -57,6 +57,45 @@ def test_nan_treatment():
     np.testing.assert_array_almost_equal(new_data.u(), new_data_drop_nan.u())
     np.testing.assert_array_almost_equal(new_data.v(), new_data_drop_nan.v())
 
+
+def test_nan_treatment_from_gridded():
+    data = WindGrid.add_time()(lon=(10,20), lat=(50,60), time=('2020-01-01 00:00', '2020-01-10 06:00'))
+    data_high = WindGrid.add_time()(lon=(10,20), lat=(50,60), time=('2020-01-01 00:00', '2020-01-10 06:00'))
+    data_nan = WindGrid.add_time()(lon=(10,20), lat=(50,60), time=('2020-01-01 00:00', '2020-01-10 06:00'))
+    data.set_spacing(nx=10, ny=11)
+    data_high.set_spacing(nx=10, ny=11)
+    data_nan.set_spacing(nx=10, ny=11)
+
+    data.set_dd(0)
+    data.set_ff(10)
+    
+    u = deepcopy(data.u())
+    v= deepcopy(data.v())
+    u[:,0] = 100
+    v[:,0] = 100
+    data_high.set_u(u)
+    data_high.set_v(v)
+
+    u[:,0] = np.nan
+    v[:,0] = np.nan
+
+    data_nan.set_u(u)
+    data_nan.set_v(v)
+
+    grid = WindGrid(lon=(10.5,19.5),lat=(50.5,59.5))
+    grid.set_spacing(nx=10, ny=11)
+
+    new_data = data.resample.grid(grid)
+    new_data_high = data_high.resample.grid(grid)
+    new_data_high_from_nan = data_nan.resample.grid(grid, mask_nan=100)
+    new_data_drop_nan = data_nan.resample.grid(grid, drop_nan=True)
+
+    np.testing.assert_array_almost_equal(new_data_high.u(), new_data_high_from_nan.u())
+    np.testing.assert_array_almost_equal(new_data_high.v(), new_data_high_from_nan.v())
+    np.testing.assert_array_almost_equal(new_data.u(), new_data_drop_nan.u())
+    np.testing.assert_array_almost_equal(new_data.v(), new_data_drop_nan.v())
+
+
 def test_scipy_dont_regrid_empty_vars():
     data = WindGrid.add_time()(
         lon=(10, 20), lat=(50, 60), time=("2020-01-01 00:00", "2020-01-10 06:00")
