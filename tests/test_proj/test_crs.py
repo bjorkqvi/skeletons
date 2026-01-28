a89bbf8b43e10828f831b3497e88325d59dcf327
Jan-Victor Björkqvist <janvb@met.no>
2026-01-28 13:37:33 +0100

    need to set metadata manager in test

--- tests/test_proj/test_crs.py
+++ tests/test_proj/test_crs.py
@@ -1,18 +1,20 @@
 from geo_skeletons.managers.proj_manager import ProjManager
+from geo_skeletons.managers.metadata_manager import MetaDataManager
+
 from pyproj import CRS
 
 def test_none():
-    pm = ProjManager(metadata_manager=None, lon=(None, None), lat=(None, None))
+    pm = ProjManager(metadata_manager=MetaDataManager(None), lon=(None, None), lat=(None, None))
     assert pm.crs() is None
 
 def test_epsg():
-    pm = ProjManager(metadata_manager=None, lon=(None, None), lat=(None, None))
+    pm = ProjManager(metadata_manager=MetaDataManager(None), lon=(None, None), lat=(None, None))
     assert pm.crs() is None
     pm.set(4326)
     assert isinstance(pm.crs(), CRS)
 
 def test_proj4():
-    pm = ProjManager(metadata_manager=None, lon=(None, None), lat=(None, None))
+    pm = ProjManager(metadata_manager=MetaDataManager(None), lon=(None, None), lat=(None, None))
     assert pm.crs() is None
     pm.set("+proj=ob_tran +o_proj=latlon +o_lat_p=45 +o_lon_p=30 +lon_0=0")
     assert isinstance(pm.crs(), CRS)
