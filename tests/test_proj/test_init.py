a0b999aba04351da2ffeb5e39cde9d54e391c51e
Jan-Victor Björkqvist <janvb@met.no>
2026-01-28 13:38:28 +0100

    need to set metadata manager in test

--- tests/test_proj/test_init.py
+++ tests/test_proj/test_init.py
@@ -1,16 +1,16 @@
 from geo_skeletons.managers.proj_manager import ProjManager
-
+from geo_skeletons.managers.metadata_manager import MetaDataManager
 
 def test_set_epsg():
-    pm = ProjManager(crs=4326, metadata_manager=None, lon=(None, None), lat=(None, None))
+    pm = ProjManager(crs=4326, metadata_manager=MetaDataManager(None), lon=(None, None), lat=(None, None))
     assert pm.crs() == 4326
     
 
 def test_set_epsg_from_str():
-    pm = ProjManager(crs='EPSG:4326', metadata_manager=None, lon=(None, None), lat=(None, None))
+    pm = ProjManager(crs='EPSG:4326',metadata_manager=MetaDataManager(None), lon=(None, None), lat=(None, None))
     assert pm.crs() == 4326
     
 
 def test_set_proj4():
-    pm = ProjManager(crs="+proj=ob_tran +o_proj=latlon +o_lat_p=45 +o_lon_p=30 +lon_0=0", metadata_manager=None, lon=(None, None), lat=(None, None))
+    pm = ProjManager(crs="+proj=ob_tran +o_proj=latlon +o_lat_p=45 +o_lon_p=30 +lon_0=0", metadata_manager=MetaDataManager(None), lon=(None, None), lat=(None, None))
     assert pm.crs() == "+proj=ob_tran +o_proj=latlon +o_lat_p=45 +o_lon_p=30 +lon_0=0"
