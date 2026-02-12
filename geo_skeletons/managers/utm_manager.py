31f874824c4d31dc5a576de9d513a52632e5f418
Jan-Victor Björkqvist <janvb@met.no>
2026-02-12 10:52:17 +0100

    removed unused code

--- geo_skeletons/managers/utm_manager.py
+++ geo_skeletons/managers/utm_manager.py
@@ -215,18 +215,3 @@ class UTMManager:
 
         return y
 
-
-# def cap_lat_for_utm(lat):
-#     if isinstance(lat, float):
-#         lat = np.array([lat])
-#     if len(lat) > 0 and max(lat) > 84:
-#         print(
-#             f"Max latitude {max(lat)}>84. These points well be capped to 84 deg in UTM conversion!"
-#         )
-#         lat[lat > 84.0] = 84.0
-#     if len(lat) > 0 and min(lat) < -80:
-#         lat[lat < -80.0] = -80.0
-#         print(
-#             f"Min latitude {min(lat)}<-80. These points well be capped to -80 deg in UTM conversion!"
-#         )
-#     return lat
